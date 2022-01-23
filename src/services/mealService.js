d6ad16591ef5a690b5fbb309cad6709a27f52ab1
Vrezerino <parkpatrick1@gmail.com>
2022-01-23 22:46:35 +0200

    test commit to test github action

--- src/services/mealService.js
+++ src/services/mealService.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import { baseUrl } from '../../utils';
+import { testMeals } from '../utils/testMeals';
 
 export const fetchMeals = async () => {
-	const response = await axios.get(`${baseUrl}/api/meals`);
-	return response.data.meals; // return meals array to dispatch
+	if (process.env.NODE_ENV === 'test') {
+		return testMeals;
+	} else {
+		const response = await axios.get(`${baseUrl}/api/meals`);
+		return response.data.meals; // return meals array to dispatch
+	}
 };
