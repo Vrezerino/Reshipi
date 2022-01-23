b722c9bd4edd14a5f8371c969400b16bf07c95cd
Vrezerino <parkpatrick1@gmail.com>
2022-01-23 18:00:54 +0200

    initial test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,6 +1,6 @@
 describe('Farm CSV database', () => {
 	it('Front page can be opened', () => {
-		cy.visit('http://localhost:3006');
+		cy.visit('http://localhost:3003');
 
 		cy.contains('Retsipi');
 		cy.contains('Find delicious recipes and filter with name and/or ingredients.');
