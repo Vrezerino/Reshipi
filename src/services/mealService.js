3df1c0f543163bcce410b2950ef1a1d7391a26a6
Vrezerino <parkpatrick1@gmail.com>
2022-01-23 23:14:53 +0200

    modify test mode

--- src/services/mealService.js
+++ src/services/mealService.js
@@ -3,7 +3,7 @@ import { baseUrl } from '../../utils';
 import { testMeals } from '../utils/testMeals';
 
 export const fetchMeals = async () => {
-	if (process.env.NODE_ENV === 'test') {
+	if (process.env.NODE_ENV === 'none') { // none = test mode
 		return testMeals;
 	} else {
 		const response = await axios.get(`${baseUrl}/api/meals`);
