ec1ddcbbd8340dc9251da50d23745daeabc8cc67
Vrezerino <parkpatrick1@gmail.com>
2022-01-24 01:02:56 +0200

    move ingredient search terms to reducer

--- src/MealList/index.jsx
+++ src/MealList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useStateValue } from '../state';
 import { setResults, setIngredientSearchTerms } from '../state';
 import { mapRecipeInfo } from '../utils/mapRecipeInfo';
