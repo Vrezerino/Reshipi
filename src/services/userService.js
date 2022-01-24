8d91998e5d189c283f2a646aabeb51eb5b9903c1
Vrezerino <parkpatrick1@gmail.com>
2022-01-24 14:35:47 +0200

    implement login/register form

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { baseUrl } from '../../utils';
 
 export const doLogin = async user => {
-	const response = axios.post(`${baseUrl}/api/login`, user);
+	const response = await axios.post(`${baseUrl}/api/login`, user);
 	return response.data;
 };
 
 export const doRegister = async user => {
-	const response = axios.post(`${baseUrl}/api/users`, user);
+	const response = await axios.post(`${baseUrl}/api/users`, user);
 	return response.data;
 };
